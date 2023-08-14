622b3bfb95337b22e124e3d29bf0e2cd0224825c
J3-Aquacool <42802228+J3-Aquacool@users.noreply.github.com>
2023-08-14 11:35:33 +0530

    Add files via upload

--- LambdaExercise.java
+++ LambdaExercise.java
@@ -1,6 +1,7 @@
 package com.methodreference.demo;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Products
@@ -67,21 +68,78 @@ public class LambdaExercise {
 		
 		
 		List<Products> product=Arrays.asList(
-				new Products(101," ", description, price, brand),
-				new Products(502,"", description, price, brand),
-				new Products(303," ", description, price, brand),
-				new Products(210," ", description, price, brand)
+				new Products(101, "Shampoo", "Long and Strong Hair", 100, "Pantine"), 
+				new Products(502, "Body wash", "Smooth and clear skin", 200, "Lux"), 
+				new Products(303, "Baby Soap", "for babies", 150, "J&J"), 
+				new Products(210, "Hand Wash", "Kills 99.9% germs", 50, "Dettol")
 				
 				
 				);
 		
 		
+
+
+		
 		// Tasks to be performed
 		// Add ur data has per ur choice to the above code
 		// 1. Sort by brands name
+		
+		Collections.sort(product,(p1,p2) -> p1.getBrand().compareTo(p2.getBrand()));
+		
+		
+		System.out.println(product);
+		
+		
+		
 		// create one method whih will print all elemnts in the list
+		
+		print(product);
+		
 		// create a method that prints all products  whose brand name starts with  p
+		
+		printCondition(product,p->p.getBrand().endsWith("P") );
+		
+		
+		printCondition(product,p->p.getBrand().startsWith("P") );
 		
 	}
 
+	
+	
+	
+	public static void print(List<Products> p)
+	{
+		
+		for(Products pro:p)
+		{
+			
+			System.out.println(pro);
+		}
+	}
+
+
+
+public static void printCondition(List<Products> p,CheckCondition c)
+{
+	
+	for(Products pro:p)
+	{
+		if(c.test(pro))
+		{
+			System.out.println(pro);
+		}
+	}
 }
+
+
+
+}
+
+
+interface CheckCondition
+{
+	
+	boolean test(Products p);
+	
+}
+
